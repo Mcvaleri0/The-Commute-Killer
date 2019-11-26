e47060f7b8e98110f987bcf69a0cc12fb1dae8a5
Manel <mmpcp23@gmail.com>
2019-11-26 18:16:43 +0000

    Basic Path finding

---
+++ The Commute Killer/Assets/Scripts/Map/MapNode.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapNode : MonoBehaviour
+{
+    public int id = 0;
+
+    public int State = 0; //[ 0 - Not visited | 1 - To visit | 2 - Visited ]
+
+    public MapNode Previous;
+
+    public List<MapNode> AdjacentNodes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(State != 0)
+        {
+            if(this.GetComponentInParent<MapController>().SearchState)
+            {
+                State = 0;
+
+                Previous = null;
+            }
+        }
+    }
+}
