9ef6edfd112b6df219963996e53b9b6b02f9abfa
Fmbnicolau <fmbnicolau@hotmail.com>
2019-12-17 01:18:32 +0000

    fire hydrant

--- The Commute Killer/Assets/Scripts/EventManager/Event.cs
+++ The Commute Killer/Assets/Scripts/EventManager/Event.cs
@@ -6,6 +6,7 @@ public enum Event
 {
     //Hydrant_0_ON, Hydrant_0_OFF,
     Hydrant_ON, Hydrant_OFF,
+    Hydrant_1_ON, Hydrant_1_OFF,
     VictimStartEnd, VictimEndStart, VictimAtGoal, VictimAtDumpster,
     InteractibleGardenGate_Close, InteractibleGardenGate_Open,
     InteractibleGardenGate_1_Close, InteractibleGardenGate_1_Open,
