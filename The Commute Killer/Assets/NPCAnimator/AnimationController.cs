9547c1c3dd1ace95bd6bf3f1607f4e4b6c36b8be
Fmbnicolau <fmbnicolau@hotmail.com>
2020-01-05 01:37:57 +0000

    small npcanimation rework test

---
+++ The Commute Killer/Assets/NPCAnimator/AnimationController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    public Player player;
+    static Animator anim;
+    protected CharacterController character;
+
+    //the number value is used in the animator
+    public enum States
+    {
+        Walking = 0,
+        Alert   = 1,
+        Dead    = 2,
+        Talking = 3
+    }
+
+    protected int State = (int) States.Walking;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        character = gameObject.GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        //pass params to animator
+        Vector3 velocity = character.velocity;
+        float speed = velocity.magnitude;
+        anim.SetFloat("Speed", 3);
+  
+    }
+
+    //set State
+    public void SetState(States state)
+    {
+        this.State = (int) state;
+        anim.SetInteger("State", State);
+    }
+
+}
